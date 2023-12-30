03461d83f260131408d75ad3087aa64bea06a9a7
Jason Maverick Diokno Serencio <202180383@psu.palawan.edu.ph>
2023-12-31 01:19:58 +0800

    added all the tables and needed field types

--- AppDev_finalProj/app/admin.py
+++ AppDev_finalProj/app/admin.py
@@ -1,19 +1,32 @@
 from django.contrib import admin
-from .models import Guardians, Students, Teachers
+from .models import Guardian, Student, Teacher, Detention, Subject
 
-@admin.register(Guardians)
+@admin.register(Guardian)
 class GuardiansAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'email', 'phone_number', 'is_emergency_contact']
     search_fields = ['first_name', 'last_name', 'email', 'phone_number']
 
-@admin.register(Students)
+@admin.register(Student)
 class StudentsAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'date_of_birth', 'email', 'admission_date', 'is_active']
+    list_display = ['first_name', 'last_name', 'date_of_birth', 'email', 'admission_date', 'guardian', 'is_active']
     search_fields = ['first_name', 'last_name', 'email', 'phone_number']
     list_filter = ['is_active', 'admission_date']
 
-@admin.register(Teachers)
+@admin.register(Teacher)
 class TeachersAdmin(admin.ModelAdmin):
     list_display = ['name', 'birthdate', 'subject', 'email', 'joining_date', 'is_active']
     search_fields = ['name', 'email', 'phone_number']
     list_filter = ['is_active', 'joining_date']
+
+@admin.register(Detention)
+class DetentionsAdmin(admin.ModelAdmin):
+    list_display = ['student', 'teacher', 'incident_time', 'fines', 'is_resolved']
+    search_fields = ['student__first_name', 'student__last_name', 'teacher__name', 'reason']
+    list_filter = ['is_resolved', 'incident_time']
+    
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ['subject_name', 'code', 'prerequisite', 'units', 'lab']
+    search_fields = ['subject_name', 'code']
+    list_filter = ['lab']
+    raw_id_fields = ['prerequisite']
--- AppDev_finalProj/app/models.py
+++ AppDev_finalProj/app/models.py
@@ -8,7 +8,7 @@ class BaseModel(models.Model):
     class Meta:
         abstract = True
 
-class Guardians(BaseModel):
+class Guardian(BaseModel):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100, null=True, blank=True)
@@ -16,37 +16,64 @@ class Guardians(BaseModel):
     address = models.TextField(null=True, blank=True)
     occupation = models.CharField(max_length=100, null=True, blank=True)
     is_emergency_contact = models.BooleanField(default=False)
+    number_of_children = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+    
+class Subject(models.Model):
+    subject_name = models.CharField(max_length=100)
+    code = models.CharField(max_length=20, unique=True)
+    prerequisite = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
+    units = models.IntegerField()
+    lab = models.BooleanField(default=False)
 
-class Students(BaseModel):
+    def __str__(self):
+        return self.subject_name
+
+class Student(BaseModel):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     date_of_birth = models.DateField(null=True, blank=True)
     email = models.EmailField(max_length=100, null=True, blank=True)
     address = models.TextField(null=True, blank=True)
     phone_number = models.CharField(max_length=20, null=True, blank=True)
-    guardian = models.ForeignKey(Guardians, on_delete=models.SET_NULL, null=True, blank=True)
+    guardian = models.ForeignKey(Guardian, on_delete=models.SET_NULL, null=True, blank=True)
     admission_date = models.DateField(null=True, blank=True)
     is_active = models.BooleanField(default=True)
-    teachers = models.ManyToManyField('Teachers', related_name='students_taught', blank=True)
+    teachers = models.ManyToManyField('Teacher', related_name='students_taught', blank=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
-class Teachers(BaseModel):
+class Teacher(BaseModel):
     name = models.CharField(max_length=100, null=True, blank=True)
     birthdate = models.DateField(null=True, blank=True)
-    subject = models.CharField(max_length=250, null=True, blank=True)
+    subject = models.ForeignKey(Subject, on_delete=models.SET_NULL, null=True, blank=True)
     email = models.EmailField(max_length=100, null=True, blank=True)
     address = models.TextField(null=True, blank=True)
     phone_number = models.CharField(max_length=20, null=True, blank=True)
     experience_years = models.PositiveIntegerField(null=True, blank=True)
     joining_date = models.DateField(null=True, blank=True)
     is_active = models.BooleanField(default=True)
-    students = models.ManyToManyField(Students, related_name='teachers_taught', blank=True)
+    students = models.ManyToManyField(Student, related_name='teachers_taught', blank=True)
+    notes = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to='teacher_images/', null=True, blank=True)
 
     def __str__(self):
         return self.name
 
+class Detention(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    incident_time = models.DateTimeField()
+    fines = models.DecimalField(max_digits=10, decimal_places=2)
+    reason = models.TextField()
+    is_resolved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.student.first_name} {self.student.last_name} Detention"
+
+    class Meta:
+        ordering = ['-incident_time']
+        
