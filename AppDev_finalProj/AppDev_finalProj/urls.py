0efa30eba9c169fdbcb294a4ef42f4c7c54f1b8a
Jason Maverick Diokno Serencio <202180383@psu.palawan.edu.ph>
2023-12-31 10:59:56 +0800

    fixed the image viewing

--- AppDev_finalProj/AppDev_finalProj/urls.py
+++ AppDev_finalProj/AppDev_finalProj/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from app import views
 from app.views import HomePageView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -18,3 +20,6 @@ urlpatterns = [
     
     path('detention_list', views.DetentionList.as_view(), name='detention-list'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)   
